79ff61992456f22d56f3fb8844f2c19b5661eab6
ZakiaHussaini <zakiahussaini19@gmail.com>
2023-01-03 20:36:35 +0000

    add data as json file

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import json
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -9,7 +11,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About" ,list_of_numbers = [1,2,4])
+    data = []
+    with open("data/company.json", "r") as json_data:
+          data = json.load(json_data)
+    return render_template("about.html", page_title="About", company = data )
 
 
 @app.route("/contact")
